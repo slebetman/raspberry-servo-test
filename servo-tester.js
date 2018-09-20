862e64eef65086b9a699c8b214e7bfac526b9bfc
adly <slebetman@gmail.com>
2018-09-21 00:30:45 +0800

    Wrote simple servo tester script

---
+++ servo-tester.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+var Gpio = require('pigpio').Gpio;
+
+var servo = new Gpio(18,{mode: Gpio.OUTPUT});
+
+var CENTER = 1560;
+
+var offset = 0;
+setInterval(function(){
+	offset += 0.05;
+	if (offset > (Math.PI*2)) {
+		offset -= (Math.PI*2);
+	}
+	
+	var position = Math.round((Math.sin(offset)/Math.PI)*2400);
+	
+	servo.servoWrite(CENTER+position);
+},20);
